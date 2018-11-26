6ecf6e6d494658cb6049036484c18978377da71d
vampory <44905458+vampory@users.noreply.github.com>
2018-11-26 16:01:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,35 +2,32 @@ const Discord = require('discord.js');
 
 const Util = require('discord.js');
     client = new Discord.Client({sisableEveryone: true})
- const devs = ['','475070652727033858'];
-const adminprefix = "T";
+ const devs = ['47507065272703385' , '' , ''];
+const adminprefix = "!";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
-      message.channel.sendMessage(`** Status has been changed to: ${argresult} **`)
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
   } else 
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(`** Status has been changed to: ${argresult}**`)
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
   } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(`** Status has been changed to: ${argresult}**`)
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
   } else 
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/Taino#6004");
-      message.channel.sendMessage(`** Status has been changed to: ${argresult} **`)
-  }
-  });
-
-client.on('ready', () => {
-
-console.log('taino');
-
-});
-
-
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
+  } else
+   if (message.content.startsWith(adminprefix + 'av')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**avatar set  : `);
+ }
+    });
+  
 client.login(process.env.BOT_TOKEN);
